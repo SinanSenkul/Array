8411da80f6a33cd9b05214e6dd1079d256c4d7fc
sinan <sinan@SINAN-SENKUL>
2024-08-16 12:07:25 +0300

    Add project files.

---
+++ Array/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+int[] numbers = new int[10];
+for (int i = 0; i < numbers.Length; i++)
+{
+    Random rnd = new Random();
+    int ranNum = rnd.Next(0, 1000);
+    numbers[i] = ranNum;
+}
+Console.WriteLine("diziye 11. tam sayı elemanı giriniz:");
+int el11th = Convert.ToInt32(Console.ReadLine());
+numbers = numbers.Concat(new int[] { el11th }).ToArray();
+Array.Sort(numbers);
+Array.Reverse(numbers);
+foreach (int i in numbers)
+{
+    Console.WriteLine(i);
+}
